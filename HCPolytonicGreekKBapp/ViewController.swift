12c861e335a1ec62eab397803a1def7b22fc29e2
Jeremy March <jeremy@jeremys-mbp.home>
2016-12-24 23:25:03 -0500

    Initial Commit

---
+++ HCPolytonicGreekKBapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HCPolytonicGreekKBapp
+//
+//  Created by Jeremy March on 12/24/16.
+//  Copyright © 2016 Jeremy March. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
