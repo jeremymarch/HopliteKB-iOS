31fc1e7c6bef51f66e1b26fb8c5168214c9b1bcf
jeremymarch <jmarch@gradcenter.cuny.edu>
2018-04-19 21:28:12 -0400

    Update copyright/license.

--- HCPolytonicGreekKBapp/AboutViewController.swift
+++ HCPolytonicGreekKBapp/AboutViewController.swift
@@ -2,13 +2,14 @@
 //  AboutViewController.swift
 //  HCPolytonicGreekKBapp
 //
+//  Created by Jeremy March on 2/24/17.
+//  Copyright © 2017 Jeremy March. All rights reserved.
+//
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 //
-//  Created by Jeremy March on 2/24/17.
-//  Copyright © 2017 Jeremy March. All rights reserved.
-//
+
 
 import UIKit
 
